aba3d8ded4ed53397aa79ee64c083e6ddb29ce9d
Martin <martinpottiez@hotmail.fr>
2021-07-07 14:46:33 +0200

    Add SectionHeader

---
+++ spacex/Model/SectionHeader.swift
@@ -0,0 +1,17 @@
+//
+//  SectionHeader.swift
+//  spacex
+//
+//  Created by Martin on 07/07/2021.
+//
+
+import UIKit
+
+class SectionHeader: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
