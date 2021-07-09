3bb5fba90f15882bffa70d2a7c4f4501ef3517f2
Martin <martinpottiez@hotmail.fr>
2021-07-09 16:34:11 +0200

    Update Launch Structure

--- spacex/Model/Launch.swift
+++ spacex/Model/Launch.swift
@@ -9,6 +9,55 @@ import Foundation
 
 struct Launch: Hashable, Decodable {
     
+    struct Fairings: Hashable, Decodable {
+        
+        enum CodingKeys: String, CodingKey {
+            case reused,
+                 recovered
+        }
+        var reused: Bool?
+        var recovered: Bool?
+    }
+    
+    struct LaunchLinks: Hashable, Decodable {
+        
+        struct Flickr: Hashable, Decodable {
+            enum CodingKeys: String, CodingKey {
+                case original
+            }
+            var original: [String]?
+        }
+        
+        struct Patch: Hashable, Decodable {
+            var small: String?
+            var large: String?
+        }
+        
+        enum CodingKeys: String, CodingKey {
+            case article,
+                 wikipedia,
+                 patch,
+                 flickr,
+                 _webcast = "webcast"
+        }
+        var patch: Patch?
+        var flickr: Flickr?
+        
+        
+        private var _webcast: String?
+        
+        
+        
+        var webcast: URL? {
+            guard let url = _webcast else {
+                return nil
+            }
+            return URL(string: url)
+        }
+        var article: URL?
+        var wikipedia: URL?
+    }
+    
     enum CodingKeys: String, CodingKey, Hashable {
         case name,
              links,
